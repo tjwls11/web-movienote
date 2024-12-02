56f829213b5015ce0e05a0b28091a0465ea78084
Eunmi <eunmi041119@gmail.com>
2024-12-02 17:37:27 +0900

    memo

--- src/app/memo/page.tsx
+++ src/app/memo/page.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export default function MemoPage() {
-  return <div className="justify-center items-center">기록장 페이지입니다.</div>
+  return (
+    <div className="justify-center items-center text-bold ">
+      기록장 페이지입니다.
+    </div>
+  )
 }
