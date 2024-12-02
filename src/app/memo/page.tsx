33f93f315df619818e3e004501017670f18b9ecb
Eunmi <eunmi041119@gmail.com>
2024-12-03 01:56:26 +0900

    기록장

--- src/app/memo/page.tsx
+++ src/app/memo/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import MemoList from '@/components/MemoList'
 
 export default function MemoPage() {
   return (
-    <div className="justify-center items-center text-bold ">
-      기록장 페이지입니다.
+    <div>
+      <h1 className="text-4xl font-bold text-green-800 mb-8">영화 기록장</h1>
+      <MemoList />
+      <MemoList />
     </div>
   )
 }
