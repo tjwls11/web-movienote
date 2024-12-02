90509974eb5926f704287cc244e5bf621f8da5e9
Eunmi <eunmi041119@gmail.com>
2024-12-02 16:15:29 +0900

    memo test

--- src/app/memo/page.tsx
+++ src/app/memo/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function MemoPage() {
-  return <div className="justify-center items-center">MemoPage</div>
+  return <div className="justify-center items-center">기록장 페이지</div>
 }
