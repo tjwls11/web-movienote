311a6e3269bf57bd6331de96fdf4cc7933a1be87
hyejeong22 <hyejung525@naver.com>
2024-12-03 10:42:26 +0900

    forkkk

--- src/app/movie/page.tsx
+++ src/app/movie/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function MoviePage() {
-  return <div className="justify-center items-center">수정수정</div>
+  return <div className="justify-center items-center">수정</div>
 }
